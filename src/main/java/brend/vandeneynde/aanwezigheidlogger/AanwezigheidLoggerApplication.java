4bef43bf7ed2d1679ef8f0e2502c58bada88fef5
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-11-21 15:00:11 +0100

    Structuur + instal van de frontend interface

---
+++ src/main/java/brend/vandeneynde/aanwezigheidlogger/AanwezigheidLoggerApplication.java
@@ -0,0 +1,13 @@
+package brend.vandeneynde.aanwezigheidlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AanwezigheidLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AanwezigheidLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brend/vandeneynde/aanwezigheidlogger/AanwezigheidLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package brend.vandeneynde.aanwezigheidlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AanwezigheidLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
