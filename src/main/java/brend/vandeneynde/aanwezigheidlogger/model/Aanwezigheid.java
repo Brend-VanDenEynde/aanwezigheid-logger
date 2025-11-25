658ef7d59d3066a06906ab6ef1201e7d10cfd012
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-11-25 22:20:30 +0100

    Student aanwezigheid gemaakt

---
+++ src/main/java/brend/vandeneynde/aanwezigheidlogger/model/Aanwezigheid.java
@@ -0,0 +1,56 @@
+package brend.vandeneynde.aanwezigheidlogger.model;
+
+import java.time.LocalDateTime;
+
+public class Aanwezigheid {
+    private int id;
+    private int studentId;
+    private LocalDateTime timestamp;
+    private String lesOfProject;
+    private String opmerking;
+
+    public Aanwezigheid() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getLesOfProject() {
+        return lesOfProject;
+    }
+
+    public void setLesOfProject(String lesOfProject) {
+        this.lesOfProject = lesOfProject;
+    }
+
+    public String getOpmerking() {
+        return opmerking;
+    }
+
+    public void setOpmerking(String opmerking) {
+        this.opmerking = opmerking;
+    }
+}
