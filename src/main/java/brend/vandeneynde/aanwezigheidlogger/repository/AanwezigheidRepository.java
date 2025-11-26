c90cb7a34331d99058cf44411f1517427039592a
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-11-26 10:37:41 +0100

    @ toegevoegd bij repository was dit vergeten

--- src/main/java/brend/vandeneynde/aanwezigheidlogger/repository/AanwezigheidRepository.java
+++ src/main/java/brend/vandeneynde/aanwezigheidlogger/repository/AanwezigheidRepository.java
@@ -2,9 +2,12 @@ package brend.vandeneynde.aanwezigheidlogger.repository;
 
 import brend.vandeneynde.aanwezigheidlogger.model.Aanwezigheid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface AanwezigheidRepository extends JpaRepository<Aanwezigheid, Integer> {
     List<Aanwezigheid> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/brend/vandeneynde/aanwezigheidlogger/repository/StudentRepository.java
+++ src/main/java/brend/vandeneynde/aanwezigheidlogger/repository/StudentRepository.java
@@ -2,7 +2,9 @@ package brend.vandeneynde.aanwezigheidlogger.repository;
 
 import brend.vandeneynde.aanwezigheidlogger.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
     Student zoekStamnr(String stamnr);
 }
