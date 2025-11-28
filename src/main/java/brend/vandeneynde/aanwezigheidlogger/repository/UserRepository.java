3c465ca9cf5f23651e4813747942e2d832c7dcc2
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-11-28 19:45:37 +0100

    Repository voor de user aangemaakt

---
+++ src/main/java/brend/vandeneynde/aanwezigheidlogger/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package brend.vandeneynde.aanwezigheidlogger.repository;
+
+import brend.vandeneynde.aanwezigheidlogger.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
